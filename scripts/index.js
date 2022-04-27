77dd6720a248981a2486b86884bebec7693bc659
Miriam Smith <miriam@smithydrafting.com.au>
2022-04-27 12:59:10 +1000

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,61 @@
+
+   let smedia=document.getElementById('smedia');
+   let ccreation=document.getElementById('ccreation');
+   let photography=document.getElementById('photography');
+   let writing=document.getElementById('writing');
+   let seo=document.getElementById('seo');
+   
+   smedia.onclick=function(){
+       let smedia_content=document.getElementById('smedia_content');
+       let ad=document.getElementById('ad');
+    if(smedia_content.style.display=='none'){
+        smedia_content.style.display='block';
+        ad.scrollIntoView();
+   }
+    else{
+        smedia_content.style.display='none';
+    };
+}
+
+    ccreation.onclick=function(){
+        let ccreation_content=document.getElementById('ccreation_content');
+        if(ccreation_content.style.display=='none'){
+            ccreation_content.style.display='block';
+       }
+        else{
+            ccreation_content.style.display='none';
+        };
+    }
+   
+    photography.onclick=function(){
+        let photography_content=document.getElementById('photography_content');
+        let bike=document.getElementById('bike');
+        if(photography_content.style.display=='none'){
+            photography_content.style.display='block';
+            bike.scrollIntoView();
+       }
+        else{
+            photography_content.style.display='none';
+        };
+    }
+
+    writing.onclick=function(){
+        let writing_content=document.getElementById('writing_content');
+        if(writing_content.style.display=='none'){
+            writing_content.style.display='block';
+       }
+        else{
+            writing_content.style.display='none';
+        };
+    }
+
+    seo.onclick=function(){
+        let seo_content=document.getElementById('seo_content');
+        if(seo_content.style.display=='none'){
+            seo_content.style.display='block';
+       }
+        else{
+            seo_content.style.display='none';
+        };
+    }
+   
