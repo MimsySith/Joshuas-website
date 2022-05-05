6be51466cc953fcb8525e9db28d1c15f62722d24
Miriam Smith <miriam@smithydrafting.com.au>
2022-05-05 11:33:30 +1000

    changes to mobile function on contact page

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,18 +1,15 @@
-let menu_icon=document.getElementById('menu');
-   let menu=document.getElementById('menu_div');
-   let list=document.getElementById('list');
-   let navbox=document.getElementById('nav');
-   let head=document.getElementById('head');
-   let welcome=document.getElementById('welcome');
+let menu_icon=document.getElementById('menu1');
+   let menu=document.getElementById('menu_div1');
+   let list=document.getElementById('list1');
+   let navbox=document.getElementById('nav1');
+
+   let welcome=document.getElementById('welcome1');
   
 
-   if(window.screen.availWidth<=800){
-    menu_icon.style.display='block';s
+   if(window.screen.availWidth<=900){
+    menu_icon.style.display='block';
     list.style.display='none';
     navbox.style.backgroundColor='#fff';
-    head.style.alignItems='flex-end'
-
-    
 
     menu_icon.onclick=function(){
         if(menu.style.display=='none'){
@@ -22,4 +19,4 @@ let menu_icon=document.getElementById('menu');
         }
     }
 
-}
+   }
